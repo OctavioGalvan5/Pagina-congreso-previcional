79322f2dc9f523656889465e6bff0aca3e2422f6
Octavio <Octaviogalvan2003@gmail.com>
2026-05-11 12:54:58 -0300

    Primer commit: Rediseño completo a estilo Congreso Académico

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/cronograma')
+def cronograma():
+    return render_template('cronograma.html')
+
+
+@app.route('/modulos_ia')
+def modulos_ia():
+    return render_template('modulos_ia.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
